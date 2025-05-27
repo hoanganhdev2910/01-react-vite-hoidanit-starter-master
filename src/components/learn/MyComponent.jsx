dbaa34499fd6b3029e854c14c07c4ef126d4d108
ĐINH HOÀNG ANH <hoanganhdev.dinh2910@gmail.com>
2025-05-27 13:33:44 +0700

    Video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,19 @@
 import "./style.css";
 const MyComponent = () => {
+  //   const hoidanit = "eric 1"; //string
+  //   const hoidanit = "123"; //number
+  //   const hoidanit = true; //boolean
+  //   const hoidanit = undefined;
+  //   const hoidanit = null;
+  //   const hoidanit = [1, 2, 3];
+  const hoidanit = {
+    name: "hoidanit",
+    age: "25",
+  };
   return (
     <>
-      <div>Eric & Hoidanit update</div>
+      <div>{JSON.stringify(hoidanit)} & Hoidanit update</div>
+      <div>{console.log("Eric")}</div>
       <div className="child" style={{ borderRadius: "10px" }}>
         child
       </div>
